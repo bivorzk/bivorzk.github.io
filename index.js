6c1b94793205cafaa89386f5e67fc6b232ff5f99
bivorzk <57004571+bivorzk@users.noreply.github.com>
2024-11-03 00:56:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ async function getRainData(url) {
 
     const data = await response.json();
 
-    // Access the current weather object and check for rain using the weathercode
+
     if (data.current_weather) {
       const weatherCode = data.current_weather.weathercode;
       
-      // Return true if the weather code indicates rain, false otherwise
+
       return weatherCode >= 50 && weatherCode <= 99;
     } else {
       console.log("Current weather data not available.");
